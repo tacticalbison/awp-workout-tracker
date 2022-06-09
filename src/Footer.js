10e2ce12863e099d98aba1e772c56688db10916d
Tim Brandau <96590473+tacticalbison@users.noreply.github.com>
2022-06-09 15:41:43 -0500

    started layout of main page

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <p>End Workout</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function Navbar() {
+  return (
+    <div className="Navigation">
+      <p>Workout</p>
+      <p>Week 1</p>
+      <p>Day 1</p>
+    </div>
+  );
+}
+
+export default Navbar;
